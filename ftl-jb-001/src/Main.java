0f3023dad7407f36c75be9b19713e2c1acc4b54f
Sonia <sonia.kostukova@mail,ru>
2022-06-19 01:55:37 +0300

    L005 - methods examples

--- ftl-jb-001/src/Main.java
+++ ftl-jb-001/src/Main.java
@@ -1,19 +1,20 @@
-import L001.HelloWorld;
-import L002.POJO;
-import L003.Animal;
-import L003.Cat;
+import animals.Cat;
 
 public class Main {
     public static void main(String[] args) {
-        HelloWorld.run();
-        POJO.run();
+//        HelloWorld.run();
+//        POJO.run();
+//
+//
+//        Cat catVar = new Cat();
+//        catVar.voice();
+//
+//
+//        L004.Cat catL004 = new L004.Cat();
+//        catL004.voice();
 
+        Cat cat = new Cat("Barsik", 20);
+        System.out.println(cat.jumpHeight());
 
-        Cat catVar = new Cat();
-        catVar.voice();
-
-
-        L004.Cat catL004 = new L004.Cat();
-        catL004.voice();
     }
 }
