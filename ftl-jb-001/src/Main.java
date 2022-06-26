5e8fd7b65dcf2ebf677f57fd6246ef9772c49e99
Sonia <sonia.kostukova@mail,ru>
2022-06-26 22:02:24 +0300

    tasks002

--- ftl-jb-001/src/Main.java
+++ ftl-jb-001/src/Main.java
@@ -1,9 +1,11 @@
 import tasks.tasks001.Numbers;
+import tasks.tasks002.NumbersDesc;
 
 public class Main {
     public static void main(String[] args) {
 
         Numbers.run();
+        NumbersDesc.run();
 
     }
 }
---
+++ ftl-jb-001/src/tasks/tasks002/NumbersDesc.java
@@ -0,0 +1,16 @@
+package tasks.tasks002;
+
+public class NumbersDesc {
+
+    private static Integer i = 5;
+
+    public static void run(){
+
+        while (i > 0) {
+            System.out.print(i + " ");
+            i--;
+        }
+
+        System.out.println(" ");
+    }
+}
